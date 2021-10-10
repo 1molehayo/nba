6bacf16d3e660522d6b726b888ce61e6201c2fa4
Olusegun Omilabu <omilabuolusegun@gmail.com>
2021-10-10 22:21:05 +0100

    refactor next config

--- next.config.js
+++ next.config.js
@@ -20,13 +20,13 @@ module.exports = {
   },
   images: {
     domains: ['localhost', `${removehttp(process.env.API_BASE_URL)}`]
-  }
-  // webpack: (config, options) => {
-  //   config.module.rules.push({
-  //     test: /\.node$/,
-  //     loader: 'node-loader'
-  //   });
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader'
+    });
 
-  //   return config;
-  // }
+    return config;
+  }
 };

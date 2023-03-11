d2576fa9a9eb3c354cf5f8b577c5ead34b3232fd
olusegun.omilabu <olusegun.omilabu@flutterwavego.com>
2023-03-11 16:12:32 +0100

    refactor about people profile

--- utility/profiles.js
+++ utility/profiles.js
@@ -1,14 +1,14 @@
 export const PROFILES = [
-  {
-    id: 'profile-01',
-    peopleId: 'people-01',
-    fullname: 'BARTHOLOMEW AGUEGBODO, ESQ',
-    bio: `Bartholomew Aguegbodo is the Chairman, Nigerian Bar Association, Ikeja Branch. He holds LL.B degree from the University of Lagos and has been called to the Nigerian Bar over two decades ago. <br/> Bartholomew, a Chartered Accountant and Senior Legal Practitioner is the Principal, Bartholomew Aguegbodo & Co., a Law Firm located in Ikeja, Lagos State. Bartholomew who is versed in several areas of law has a special penchance for criminal litigation. He has over the years rendered probono legal services to needy Nigerians. He is a great enthusiast for legal, social and public interest issues.`
-  },
-  {
-    id: 'profile-01',
-    peopleId: 'people-02',
-    fullname: 'ADEYEMI ABIJO, ESQ',
-    bio: `Adeyemi ABIJO is a quintessential Legal Practitioner who holds an LL.M degree in Business Law, having earlier bagged his LL.B Honours Degree from the Lagos State University (LASU) Ojo, Lagos-Nigeria. <br/> Adeyemi is a co-founder of The LegalHub Partnership(LHP), a full service Law Firm located within the commercial hub of Lagos State, Ikeja. As the Lead Partner in the Firm, Adeyemi heads the Litigation and General Dispute Resolution Team of the LHP. His other core competence areas include Fundamental Human Rights and Public Interest Litigation, Real Estate & Infrastructure, Family Law & Probate. <br/> Adeyemi has provided legal advisory on several high networth transactions and has offered expert legal services to Clients in diverse areas of law. <br/> He is the Secretary of the Nigerian Bar Association, Ikeja Branch, Member, International Bar Association amongst other professional bodies. He is a Fellow of the Institute of Management Consultants. He is a member of the Rotary International, District 9110 and Past-President, Rotary Club of Ogba.`
-  }
+  // {
+  //   id: 'profile-01',
+  //   peopleId: 'people-01',
+  //   fullname: 'BARTHOLOMEW AGUEGBODO, ESQ',
+  //   bio: `Bartholomew Aguegbodo is the Chairman, Nigerian Bar Association, Ikeja Branch. He holds LL.B degree from the University of Lagos and has been called to the Nigerian Bar over two decades ago. <br/> Bartholomew, a Chartered Accountant and Senior Legal Practitioner is the Principal, Bartholomew Aguegbodo & Co., a Law Firm located in Ikeja, Lagos State. Bartholomew who is versed in several areas of law has a special penchance for criminal litigation. He has over the years rendered probono legal services to needy Nigerians. He is a great enthusiast for legal, social and public interest issues.`
+  // },
+  // {
+  //   id: 'profile-01',
+  //   peopleId: 'people-02',
+  //   fullname: 'ADEYEMI ABIJO, ESQ',
+  //   bio: `Adeyemi ABIJO is a quintessential Legal Practitioner who holds an LL.M degree in Business Law, having earlier bagged his LL.B Honours Degree from the Lagos State University (LASU) Ojo, Lagos-Nigeria. <br/> Adeyemi is a co-founder of The LegalHub Partnership(LHP), a full service Law Firm located within the commercial hub of Lagos State, Ikeja. As the Lead Partner in the Firm, Adeyemi heads the Litigation and General Dispute Resolution Team of the LHP. His other core competence areas include Fundamental Human Rights and Public Interest Litigation, Real Estate & Infrastructure, Family Law & Probate. <br/> Adeyemi has provided legal advisory on several high networth transactions and has offered expert legal services to Clients in diverse areas of law. <br/> He is the Secretary of the Nigerian Bar Association, Ikeja Branch, Member, International Bar Association amongst other professional bodies. He is a Fellow of the Institute of Management Consultants. He is a member of the Rotary International, District 9110 and Past-President, Rotary Club of Ogba.`
+  // }
 ];
